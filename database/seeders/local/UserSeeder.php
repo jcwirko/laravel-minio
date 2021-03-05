1e299aee318df9d8fbc2711a7943f3cd17ee8264
juanblariza <juanblariza@gmail.com>
2021-03-05 09:19:59 -0300

    Modifica el seeder de usuario

--- database/seeders/local/UserSeeder.php
+++ database/seeders/local/UserSeeder.php
@@ -13,41 +13,6 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        User::create([
-            'first_name' => 'Test',
-            'last_name' => 'Test',
-            'birthdate' => '1995-01-23',
-            'email' => 'test@test.com',
-            'password' => Hash::make('secret')
-        ]);
-
-        $admin = User::create([
-            'first_name' => 'Admin',
-            'last_name' => 'Admin',
-            'birthdate' => '1988-08-16',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('secret')
-        ]);
-
-        Bouncer::allow(RolesValues::ADMIN['name'])->to(AbilitiesValues::ADMINISTRATION_MODULE['name']);
-        Bouncer::allow(RolesValues::ADMIN['name'])->to(AbilitiesValues::UPDATE_ROLES['name']);
-        Bouncer::assign(RolesValues::ADMIN['name'])->to($admin);
-
-        User::factory(10000)->create();
-        User::factory(10000)->create();
-        User::factory(10000)->create();
-
-        User::create([
-            'first_name' => 'Admin',
-            'last_name' => 'Admin',
-            'birthdate' => '1988-08-16',
-            'email' => 'return@gmail.com',
-            'password' => Hash::make('secret')
-        ]);
-
-        User::factory(10000)->create();
-        User::factory(10000)->create();
-        User::factory(10000)->create();
-
+        User::factory(10)->create();
     }
 }
